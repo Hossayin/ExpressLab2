4ad5381b1cab77b694baaeee6241e2b87db9458c
Ali Hossein <alihossein96@gmail.com>
2019-12-02 15:10:56 -0500

    working on getting database back

--- routes.js
+++ routes.js
@@ -1,58 +1,75 @@
 const express = require("express");
+const pool = require("./pg-connection-pool");
 const cartItemsRoute = express.Router();
 
-cartItemsRoute.get("/", (req, res) => {
-  res.send("here are all the items");
-});
-
-const cartItems = [
-  { id: 0, product: "product1", price: 1, quantity: 1 },
-  { id: 1, product: "product2", price: 2, quantity: 2 }
-];
-let nextItem = 2;
+// cartItemsRoute.get("/", (req, res) => {
+//   let sql = "SELECT * FROM shopping_carts";
+//   pool.query(sql).then(result => {
+//     res.json(result.rows);
+//   });
+// });
 
 cartItemsRoute.get("/cart-items", (req, res) => {
-  res.json(cartItems);
-  res.status(200);
+  let sql = "SELECT * FROM shopping_cart";
+  pool.query(sql).then(result => {
+    res.json(result.rows);
+    res.status(200);
+  });
 });
 
 cartItemsRoute.get("/cart-items/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  let item = cartItems.find(aItem => aItem.id === id);
-  if (item) {
-    res.json(item);
-    res.status(200);
-  } else {
-    res.status(404);
-    res.send(`ID NOT FOUND ${id}`);
-  }
+  let sql = "SELECT * FROM shopping_cart WHERE id=$1::int";
+  let params = [id];
+  pool.query(sql, params).then(result => {
+    if (result.rows.length !== 0) {
+      res.json(result.rows[0]);
+      res.status(200);
+    } else {
+      res.status(404);
+      res.send(`ID NOT FOUND ${id}`);
+    }
+  });
 });
 cartItemsRoute.post("/cart-items", (req, res) => {
-  const item = req.body;
-  item.id = nextItem;
-  nextItem++;
-  cartItems.push(item);
-  res.status(201);
-  res.json();
+  const shopping_cart = req.body;
+  let sql = `INSERT INTO shopping_cart(product, price, quantity)
+VALUES ($1::TEXT, $2::MONEY, $3::SMALLINT)RETURNING *`;
+  let params = [
+    shopping_cart.product,
+    shopping_cart.price,
+    shopping_cart.quantity
+  ];
+  pool.query(sql, params).then(result => {
+    res.status(201);
+    res.json(result.rows[0]);
+  });
 });
 
 cartItemsRoute.put("/cart-items/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const item = req.body;
-  item.id = id;
-  const index = cartItems.findIndex(i => i.id === id);
-  cartItems.splice(index, 1, item);
-  res.status(200);
-  res.json(item);
+  const shopping_cart = req.body;
+
+  let sql =
+    "UPDATE shopping_cart SET $1::TEXT, $2::MONEY, $3::SMALLINT WHERE id=$4::int";
+  let params = [
+    shopping_cart.product,
+    shopping_cart.price,
+    shopping_cart.quantity,
+    id
+  ];
+  pool.query(sql, params).then(result => {
+    res.status(200);
+    res.json(result.rows[0]);
+  });
 });
 
 cartItemsRoute.delete("/cart-items/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const index = cartItems.findIndex(i => i.id === id);
-  if (index !== -1) {
-    cartItems.splice(index, 1);
-  }
-  res.sendStatus(204);
+  let sql = "DELETE FROM shopping_cart WHERE id=$1::int ";
+  pool.query(sql, params).then(() => {
+    res.sendStatus(204);
+  });
 });
 
 module.exports = cartItemsRoute;
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const app = express();
 const cartItemsRoute = require("./routes");
 
-const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", cartItemsRoute);
 
-const port = 3000;
+const port = 4200;
 app.listen(port, () => {
   console.log(`server started http://localhost:${port}`);
 });
